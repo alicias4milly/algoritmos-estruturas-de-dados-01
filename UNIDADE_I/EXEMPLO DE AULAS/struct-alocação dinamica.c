c07d6b8848bb81ff67c6990ff3975a31fb3b1054
alicia samilly <aliciasamilly456@gmail.com>
2024-07-29 19:09:16 -0300

    atualização

---
+++ UNIDADE_I/EXEMPLO DE AULAS/struct-alocação dinamica.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define QUANTIDADE_ALUNOS 1 //Constante 
+
+typedef struct aluno {
+    int matricula; 
+    int idade;
+    char nome[20];
+}Aluno; 
+
+void preenche(Aluno * aluno, int tamanho){
+    int count; 
+    for(count=0; count < tamanho; count++){
+    printf("INFORME O NOME: \t");
+    scanf(" %[^\n]", aluno[count].nome);
+    printf("INFORME O NUMERO DA MATRICULA: \t");
+    scanf("%d", &aluno[count].matricula);
+    printf("DIGITE A IDADE: ");
+    scanf("%d", &aluno[count].idade);
+    } 
+}
+void imprime(Aluno * aluno){
+    printf("NOME:%s\n, idade:%d\n, matricula:%d", 
+    aluno->nome, aluno->idade, aluno->matricula);
+}
+
+int main(void){
+    
+    Aluno * aluno = (Aluno*) malloc(QUANTIDADE_ALUNOS*sizeof(Aluno));
+    if(aluno==NULL){
+        printf("NO memory RAM.");
+        exit(1);
+    }
+    
+    preenche(aluno, QUANTIDADE_ALUNOS);
+    
+    int novo_tamanho;
+    printf("Informe o novo tamanho do vetor: \t");
+    scanf("%d", &novo_tamanho);
+    aluno =(Aluno*) realloc(aluno, novo_tamanho*sizeof(Aluno));
+    if(aluno==NULL){
+        exit(1);
+    } 
+    else{
+        printf("vetor realocado:\n");
+    }
+    preenche(aluno, novo_tamanho);
+    imprime(aluno);
+    return 0; 
+}
---
+++ UNIDADE_I/LISTA07_STRUC_ENUM_UNIOM/ENUM-exemplos/quest01-aula.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+typedef enum Genero{
+    MASCULINO,
+    FEMININO,
+    NEUTRO
+}genero;
+ 
+ typedef struct Pessoa{
+    char nome[20];
+    int idade;
+    genero Genero;
+ }pessoa;
+
+ int main()
+ return 0;
