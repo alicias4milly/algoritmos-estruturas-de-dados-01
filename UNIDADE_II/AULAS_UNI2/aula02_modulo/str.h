f6349359b5e556acdba4aa72cc1f53613a06cba7
alicia samilly <aliciasamilly456@gmail.com>
2024-08-03 07:47:52 -0300

    att

---
+++ UNIDADE_II/AULAS_UNI2/aula02_modulo/str.h
@@ -0,0 +1,15 @@
+/*Funções oferecidas pelo modulo str.c*/
+
+/*FUNÇÃO COMPRIMENTO
+**RETORNA O NÚMERO DE CARACTERES DE STRNG PASSADA 
+COMO PARÂMETRO*/
+int comprimento (char* s);
+
+/*FUNÇÃO COPIA
+**RETORNA OS CARACTERES DA STRING ORIG(ORIGEM) PARA 
+DEST(DESTINO)*/
+void copia (char* dest, char* orig);
+
+/*FUNÇÃO CONCATENA A STRING ORIG(ORIGEM) NA STRING
+ DEST (DESTINO)*/
+void concatena (char* dest, char* orig);
